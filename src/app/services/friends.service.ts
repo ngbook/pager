f92c70e574ad4ccbead43272f7db0a1222b3be85
jsongo <jsongo@qq.com>
2018-06-24 20:42:01 +0800

    drop useless code

--- src/app/services/friends.service.ts
+++ src/app/services/friends.service.ts
@@ -1,12 +1,6 @@
-import {
-    Http, Response,
-    Headers, RequestOptions
-} from '@angular/http';
+
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/empty';
 
 import { RequestBase } from './net-base';
 
